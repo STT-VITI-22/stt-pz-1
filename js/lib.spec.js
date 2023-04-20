6465ebff962dc542d29c8db2800c43a9d3494468
M.Reshetnikov <misha.reschetnicko@gmail.com>
2023-04-20 19:21:45 +0300

    completed all the functions from the file lib.js

--- js/lib.spec.js
+++ js/lib.spec.js
@@ -45,5 +45,59 @@ describe('Test suite for testing lib.js', () => {
       assert.deepEqual(removeByName(['misha','koval','porohenko', 'chepras007'],'cheprasMakbook'),['misha','koval','porohenko','chepras007'])
     });
   });
+  describe('Test suite for testing function calculate factorial of number', () => {
+    it('calculate factorial of number - n', () => {
+      assert.equal(factorial(1),1)
+      // assert.equal(factorial(22),1124000727777607680000)
+      // assert.equal(factorial(24),620448401733239439360000)
+    });
+  });
+  describe('Test suite for function calculate Fibonacci sequence', () => {
+    it('calculate Fibonacci sequence', () => {
+      assert.equal(fibonacci(5),5)
+      assert.equal(fibonacci(12),144)
+      assert.equal(fibonacci(11),89)
+    });
+  });
+  describe('Test suite for  function create counter', () => {
+    it('should return a function that returns incremented values of the provided count', () => {
+      const test = makeCounter(177);
+      assert.equal(test(), 177);
+      assert.equal(test(), 178);
+      assert.equal(test(), 179);
+      // assert.equal(test(), 189);
+    });
+  });
+  describe('Test suite for function create async timeout and return unixtime like timer Id', () => {
+    it('checking if it returns a valid timerId after the timer has elapsed for 1 second', () => {
+      const to = setTimeout(()=> assert.isAbove(getAsyncTimerId(1000),0), 1000);
+    });
+  });
+  describe('Test suite for function return promise and multiply paraments', () => {
+    it('calls the testAsyncMultiply function inside which your function is called, after which it waits for a response, the testAsyncMultiply function multiplied correctly, if so, it outputs', () => {
+      async function testAsyncMultiply() {
+        const result = await asyncMultiply(5);
+        if (result === 10) {
+          console.log(`Done result =${result}`);
+        } else {
+          console.error('Erorrr/');
+        }
+      }
+      testAsyncMultiply();
+    });
+  });
+  describe('Test suite for function create GET http request to server', () => {
+    it('return successful get request', async () => {
+      const result = await httpGet('https://jsonplaceholder.typicode.com/todos/1');
+      assert.isObject(JSON.parse(result));
+    });
+    it('return error get request', async () => {
+      try {
+        await httpGet('https://jsonplaceholder.typicode.com/notfound');
+      } catch (error) {
+        assert.equal(error.code, 404);
+      }
+    });
+  });
 });
 
