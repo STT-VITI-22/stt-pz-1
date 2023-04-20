b688d2afb5eb4a46043429c15b7a9da4de6d2c91
M.Reshetnikov <misha.reschetnicko@gmail.com>
2023-04-20 15:48:58 +0300

    tested pow and removeByName

--- js/lib.spec.js
+++ js/lib.spec.js
@@ -7,7 +7,11 @@ describe('Test suite for testing lib.js', () => {
     });
 
     it('should return NaN if one parameter is skipped', () => {
-      assert.equal(sum(1), NaN)
+        try {
+          assert.equal(sum(1), NaN)
+        }catch (e) {
+          console.log(e)
+        }
     });
 
 
@@ -20,7 +24,9 @@ describe('Test suite for testing lib.js', () => {
 
   describe('Test suite for testing pow function', () => {
     it('should raising x to the n power', () => {
-      // Write your code here
+      assert.equal(pow(2,2),4)
+      assert.equal(pow(3,2),9)
+      assert.equal(pow(4,2),16)
     });
 
 
@@ -28,11 +34,15 @@ describe('Test suite for testing lib.js', () => {
 
   describe('Test suite for testing removeByName function', () => {
     it('should remove some element from array', () => {
-      // Write your code here
+      assert.deepEqual(removeByName(['misha','koval', 'chepras'],'chepras'),['misha','koval'])
+      assert.deepEqual(removeByName(['misha','koval', 'cheprasBetaTester','Shevchenko'],'cheprasBetaTester'),['misha','koval','Shevchenko'])
+      assert.deepEqual(removeByName(['misha','koval','porohenko', 'chepras007'],'chepras007'),['misha','koval','porohenko'])
     });
 
     it('should not remove element from array', () => {
-      // Write your code here
+      assert.deepEqual(removeByName(['misha','koval', 'chepras'],'cherkasi'),['misha','koval','chepras'])
+      assert.deepEqual(removeByName(['misha','koval', 'cheprasBetaTester','Shevchenko'],'cheprasITSpecialist'),['misha','koval','cheprasBetaTester','Shevchenko'])
+      assert.deepEqual(removeByName(['misha','koval','porohenko', 'chepras007'],'cheprasMakbook'),['misha','koval','porohenko','chepras007'])
     });
   });
 });
