a1fe6cdfa0c06172ad983a2b0b002c524ae83bce
Anastasia21O <virstanastya21@gmail.com>
2023-04-20 19:52:06 +0300

    (fix) add all test

--- js/lib.spec.js
+++ js/lib.spec.js
@@ -49,6 +49,73 @@ describe('Test suite for testing lib.js', () => {
       assert.equal(arrayEquals(expected, result), true)
     });
   });
+
+  describe('Test suite for testing factorial function', () => {
+    it('should take factorial', () => {
+      assert.equal(factorial(3), 6)
+      assert.equal(factorial(4), 24)
+      assert.equal(factorial(5), 120)
+    });
+  });
+
+  describe('Test suite for testing fibonacci function', () => {
+    it('fibonacci', () => {
+      assert.equal(fibonacci(3), 2)
+      assert.equal(fibonacci(4), 3)
+      assert.equal(fibonacci(5), 5)
+    });
+  });
+
+  describe('Test suite for testing makeCounter function', () => {
+    it('makeCounter (+ 1)', () => {
+      let tmp = makeCounter(3);
+      assert.equal(tmp(), 3);
+      assert.equal(tmp(), 4);
+      assert.equal(tmp(), 5);
+    });
+
+    it("should return function", function() {
+      assert.equal(typeof makeCounter(0), "function");
+    });
+  });
+
+  describe('Test suite for testing getAsyncTimerId function', () => {
+    it('getAsyncTimerId return number', () => {
+      let tmp = getAsyncTimerId(1000)
+      setTimeout(function() {
+        assert.isNumber(tmp);
+      }, 2000);
+    });
+  });
+
+  describe('Test suite for testing asyncMultiply function', function() {
+    it('should return the correct value', async function() {
+      let tmp = asyncMultiply(5)
+      setTimeout(function() {
+        assert.equal(tmp, 10);
+      }, 2000);
+    });
+
+    it('should return number', async function() {
+      let tmp = asyncMultiply(5)
+      setTimeout(function() {
+        assert.isNumber(tmp);
+      }, 2000);
+    });
+  });
+
+  describe('Test suite for testing httpGet function', function() {
+    it('should return a Promise object', function() {
+      let tmp = httpGet("www.google.com");
+      assert.isString(tmp);
+    });
+    it('should return the response on a successful request', function() {
+      httpGet('https://jsonplaceholder.typicode.com/todos/1')
+        .then(function(response) {
+          assert.isString(response);
+        });
+    });
+  });
 });
 
 function arrayEquals(a, b) {
