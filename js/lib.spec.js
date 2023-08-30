62556644e9840a3a6c266df30615dee992c6962c
Andrii Savchenko <savagamer330@gmail.com>
2023-08-30 12:15:44 +0300

    tests are successful

--- js/lib.spec.js
+++ js/lib.spec.js
@@ -7,9 +7,8 @@ describe('Test suite for testing lib.js', () => {
     });
 
     it('should return NaN if one parameter is skipped', () => {
-      assert.equal(sum(1), NaN)
-    });
-
+      assert.isTrue(isNaN(sum(1)));
+    });    
 
     it('should convert string to number if input parameters is string', () => {
       assert.equal(sum('5', '1'), 6)
@@ -20,20 +19,21 @@ describe('Test suite for testing lib.js', () => {
 
   describe('Test suite for testing pow function', () => {
     it('should raising x to the n power', () => {
-      // Write your code here
+        assert.equal(pow(2, 3), 8);
+        assert.equal(pow(2, 0), 1);
+        assert.equal(pow(3, 2), 9);
     });
-
-
   });
 
   describe('Test suite for testing removeByName function', () => {
     it('should remove some element from array', () => {
-      // Write your code here
+        let list = ["John", "Jane", "Doe"];
+        assert.deepEqual(removeByName(list, "Jane"), ["John", "Doe"]);
     });
 
     it('should not remove element from array', () => {
-      // Write your code here
+        let list = ["John", "Jane", "Doe"];
+        assert.deepEqual(removeByName(list, "Smith"), ["John", "Jane", "Doe"]);
     });
   });
 });
-
