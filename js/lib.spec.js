fc3829ac36213f12ee58e37d084deae40075847e
Feryabov <eferyabov13@gmail.com>
2023-12-05 19:42:11 +0200

    (feat)pz1.1 Feryabov

--- js/lib.spec.js
+++ js/lib.spec.js
@@ -7,7 +7,12 @@ describe('Test suite for testing lib.js', () => {
     });
 
     it('should return NaN if one parameter is skipped', () => {
-      assert.equal(sum(1), NaN)
+      try {
+        const result = sum(1);
+        assert.equal(isNaN(result), true);
+      } catch (error) {
+        console.log(error);
+      }      
     });
 
 
@@ -20,7 +25,9 @@ describe('Test suite for testing lib.js', () => {
 
   describe('Test suite for testing pow function', () => {
     it('should raising x to the n power', () => {
-      // Write your code here
+      assert.equal(pow(1, 2), 1)
+      assert.equal(pow(-10,3),-1000)
+      assert.equal(pow(10,2), 100)
     });
 
 
@@ -28,12 +35,94 @@ describe('Test suite for testing lib.js', () => {
 
   describe('Test suite for testing removeByName function', () => {
     it('should remove some element from array', () => {
-      // Write your code here
+      const originalNames = ['Євген', 'Володимир', 'Сергій'];
+      const nameToRemove = 'Євген';
+      const expectedNames = ['Володимир', 'Сергій'];
+
+      const resultNames = removeByName(originalNames, nameToRemove);
+      assert.equal(arrayEquals(expectedNames, resultNames), true);
     });
 
     it('should not remove element from array', () => {
-      // Write your code here
+      const originalNames = ['Євген', 'Володимир', 'Сергій'];
+      const nameToRemove = 'Роман';
+      const expected = ['Євген', 'Володимир', 'Сергій'];
+      const resultNames = removeByName(originalNames, nameToRemove);
+      assert.equal(arrayEquals(expected, resultNames), true)
+    });
+  });
+
+  describe('Test suite for testing factorial function', () => {
+    it('should take factorial', () => {
+      assert.equal(factorial(3), 6)
+      assert.equal(factorial(4), 24)
+      assert.equal(factorial(5), 120)
+    });
+  });
+
+  describe('Test suite for testing fibonacci function', () => {
+    it('fibonacci', () => {
+      assert.equal(fibonacci(3), 2)
+      assert.equal(fibonacci(4), 3)
+      assert.equal(fibonacci(5), 5)
+    });
+  });
+
+  describe('Test suite for testing makeCounter function', () => {
+    it('makeCounter (+ 1)', () => {
+      let tmp = makeCounter(3);
+      assert.equal(tmp(), 3);
+      assert.equal(tmp(), 4);
+      assert.equal(tmp(), 5);
+    });
+
+    it("should return function", function() {
+      assert.equal(typeof makeCounter(0), "function");
+    });
+  });
+
+  describe('Test suite for testing getAsyncTimerId function', () => {
+    it('getAsyncTimerId return number', () => {
+      let tmp = getAsyncTimerId(1000)
+      setTimeout(function() {
+        assert.isNumber(tmp);
+      }, 3000);
+    });
+  });
+
+  describe('Test suite for testing asyncMultiply function', function() {
+    it('should return the correct value', async function() {
+      let tmp = asyncMultiply(5)
+      setTimeout(function() {
+        assert.equal(tmp, 10);
+      }, 3000);
+    });
+
+    it('should return number', async function() {
+      let tmp = asyncMultiply(5)
+      setTimeout(function() {
+        assert.isNumber(tmp);
+      }, 3000);
+    });
+  });
+
+  describe('Test suite for testing httpGet function', function() {
+    it('should return a Promise object', function() {
+      let tmp = httpGet("www.google.com");
+      assert.instanceOf(tmp, Promise);
+    });
+    it('should return the response on a successful request', function() {
+      httpGet('https://jsonplaceholder.typicode.com/todos/1')
+        .then(function(response) {
+          assert.isString(response);
+        });
     });
   });
 });
 
+function arrayEquals(x, y) {
+  return Array.isArray(x) &&
+    Array.isArray(y) &&
+    x.length === y.length &&
+    x.every((val, index) => val === y[index]);
+}
